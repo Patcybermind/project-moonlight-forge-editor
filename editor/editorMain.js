b1d335bdbb42004ecb32a985433fc81dd5022b38
Patcybermind <pato.gonzalezcabrera@gmail.com>
2024-06-28 16:29:40 -0400

    added export button and render though they both do the same thing

--- editor/editorMain.js
+++ editor/editorMain.js
@@ -1,19 +1,28 @@
-// contains code for buttons and controls everything
-
 import { exportSTL } from './openscadMain.js';
-//import getEditorContent from './monacoEditorMain.js';
+import { returnSTL } from './openscadMain.js';
 
-console.log("Editor Main is loaded");
 function renderFunction() {
     console.log("Render function is called");
     const code = window.getEditorContent();
     console.log(code);
     exportSTL(code);
-    
-    
 }
 
 
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('renderBtn').addEventListener('click', renderFunction);
+});
+
+
+
+function exportFunction() {
+    console.log("export function is called");
+    const code = window.getEditorContent();
+    console.log(code);
+    exportSTL(code);
+}
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('exportBtn').addEventListener('click', exportFunction);
 });
--- editor/openscadMain.js
+++ editor/openscadMain.js
@@ -30,4 +30,29 @@ export async function exportSTL(codeInput) {
     }
 }
 
+export async function returnSTL(codeInput) {
+    let filename = "tetrahedron.stl";
+    try {
+        // Await the asynchronous initialization of OpenSCAD
+        const instance = await OpenSCAD({noInitialRun: true});
+        if (!instance || !instance.FS) {
+            console.error("Failed to initialize OpenSCAD or FS is not available");
+            return;
+        }
+        instance.FS.writeFile("/input.scad", codeInput);
+
+        instance.callMain(["/input.scad", "--enable=manifold", "-o", filename]);
+        const output = instance.FS.readFile("/" + filename);
+
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(new Blob([output], { type: "application/octet-stream" }));
+        link.download = filename;
+        document.body.append(link);
+        link.click();
+        link.remove();
+    } catch (error) {
+        console.error("Error exporting STL:", error);
+    }
+}
+
 // Credit to https://github.com/hackwin/openscad-wasm-simple-demo hackwin for making a simple demo from which i am able to base this off of for the openscad
